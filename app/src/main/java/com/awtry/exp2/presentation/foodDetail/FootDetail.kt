73b4f1df0be79a087970cca52df339bfaa5ad1e6
Awtry <jorge_verah@outlook.es>
2021-10-30 15:15:58 -0500

    Agregado detalle, parte diseño
    Aun falta agrgar la parte de los datos para mostrar, pero ya esta establecida solo hará fala hacer los bindings

---
+++ app/src/main/java/com/awtry/exp2/presentation/foodDetail/FootDetail.kt
@@ -0,0 +1,18 @@
+package com.awtry.exp2.presentation.foodDetail
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.awtry.exp2.R
+import com.awtry.exp2.core.presentation.BaseFragment
+
+class FootDetail : BaseFragment(R.layout.foot_detail_fragment) {
+    override fun setBinding(view: View) {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ app/src/main/java/com/awtry/exp2/presentation/foodDetail/FootDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.awtry.exp2.presentation.foodDetail
+
+import androidx.lifecycle.ViewModel
+
+class FootDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
